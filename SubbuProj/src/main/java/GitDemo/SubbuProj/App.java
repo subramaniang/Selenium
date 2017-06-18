a403ea6101bc4e670108ab676bab82f9ca13c94a
subramaniang <subuinbox@gmail.com>
2017-06-18 13:05:31 +0530

    commit3 subbu

--- SubbuProj/src/main/java/GitDemo/SubbuProj/App.java
+++ SubbuProj/src/main/java/GitDemo/SubbuProj/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome subbu updated2" );
+        System.out.println( "Welcome subbu updated3" );
     }
 }
